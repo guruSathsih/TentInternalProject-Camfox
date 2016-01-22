ff0e87546b4ed00cee06a9d719402a5634c5ab1a
divyatent <divya_sanker@yahoo.in>
2016-01-22 15:56:35 +0530

    Deleted all files to  move phonegap app

--- camfoxconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_name="tentinternal";
-$db_host=" tentinternal.db.7752695.hostedresource.com";
-$db_port="";
-$db_username="tentinternal";
-$db_password="Cre@tusTent1";
-$hr_email=array("denzil@tentsoftware.com","ravi@tentsoftware.com");
-$siteurl="http://creatustent.com/camfox";
-?>
