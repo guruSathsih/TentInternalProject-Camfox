8360ea1e6fe224e49a11a089bf26e9dc262ede25
divyatent <divya_sanker@yahoo.in>
2016-02-03 17:05:40 +0530

    testing audio files

--- www/js/camera.js
+++ www/js/camera.js
@@ -90,3 +90,29 @@
     function onFail(message) {
       alert('Failed because: ' + message);
     }
+	
+	// This is capturing audio files
+	
+	function captureSuccess(mediaFiles) {
+		alert("inside success");
+        var i, len;
+        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+           // uploadFile(mediaFiles[i]);
+        }       
+    }
+
+    // Called if something bad happens.
+    // 
+    function captureError(error) {
+        var msg = 'An error occurred during capture: ' + error.code;
+        navigator.notification.alert(msg, null, 'Uh oh!');
+    }
+
+    // A button will call this function
+    //
+    function captureAudios() {
+        // Launch device audio recording application, 
+        // allowing user to capture up to 2 audio clips
+		alert("inside capture");
+        navigator.device.capture.captureAudio(captureSuccess, captureError, {limit: 2});
+    }
